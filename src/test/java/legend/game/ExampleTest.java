2f7191d58516459e446cfac136cef5c1ece66215
Angelo Cali <eu.angelocali@gmail.com>
2022-05-09 20:13:25 +0200

    add junit and config gradle

---
+++ src/test/java/legend/game/ExampleTest.java
@@ -0,0 +1,16 @@
+package legend.game;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void shouldPass() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void shouldFail() {
+        Assertions.assertTrue(true);
+    }
+}
