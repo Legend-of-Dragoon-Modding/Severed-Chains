4b7baadf378888caecd4bdead2595cbffaf7d4d3
Corey Frenette <corey@narwhunderful.com>
2022-05-15 19:29:19 -0300

    Up to S_EFFE now

--- src/main/java/legend/game/types/BttlScriptData6c.java
+++ src/main/java/legend/game/types/BttlScriptData6c.java
@@ -1,9 +1,10 @@
 package legend.game.types;
 
 import legend.core.memory.Value;
-import legend.core.memory.types.FunctionRef;
+import legend.core.memory.types.BiFunctionRef;
 import legend.core.memory.types.MemoryRef;
 import legend.core.memory.types.Pointer;
+import legend.core.memory.types.ShortRef;
 import legend.core.memory.types.TriConsumerRef;
 import legend.core.memory.types.UnsignedIntRef;
 
@@ -12,7 +13,13 @@ public class BttlScriptData6c implements MemoryRef {
 
   public final Pointer<BttlScriptData6c> _00;
   public final UnsignedIntRef _04;
-  public final Pointer<FunctionRef<BttlScriptData6c, Long>> _08;
+  public final Pointer<BiFunctionRef<BttlScriptData6c, BttlScriptData6c, Long>> _08;
+  /** TODO */
+  public final Value _0c;
+
+  public final ShortRef _12;
+  public final UnsignedIntRef _14;
+  public final UnsignedIntRef _18;
 
   public final Pointer<TriConsumerRef<Integer, ScriptState<BttlScriptData6c>, BttlScriptData6c>> _48;
 
@@ -23,7 +30,12 @@ public class BttlScriptData6c implements MemoryRef {
 
     this._00 = ref.offset(4, 0x00L).cast(Pointer.deferred(4, BttlScriptData6c::new));
     this._04 = ref.offset(4, 0x04L).cast(UnsignedIntRef::new);
-    this._08 = ref.offset(4, 0x08L).cast(Pointer.deferred(4, FunctionRef::new));
+    this._08 = ref.offset(4, 0x08L).cast(Pointer.deferred(4, BiFunctionRef::new));
+    this._0c = ref.offset(4, 0x0cL);
+
+    this._12 = ref.offset(2, 0x12L).cast(ShortRef::new);
+    this._14 = ref.offset(4, 0x14L).cast(UnsignedIntRef::new);
+    this._18 = ref.offset(4, 0x18L).cast(UnsignedIntRef::new);
 
     this._48 = ref.offset(4, 0x48L).cast(Pointer.deferred(4, TriConsumerRef::new));
 
