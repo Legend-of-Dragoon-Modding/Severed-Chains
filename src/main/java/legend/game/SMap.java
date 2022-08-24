e58d893be469b005bed9db1624792cda7b6e4cef
Corey Frenette <corey@narwhunderful.com>
2022-08-23 21:41:35 -0300

    Fix /0 error

--- src/main/java/legend/game/SMap.java
+++ src/main/java/legend/game/SMap.java
@@ -7709,7 +7709,14 @@ public final class SMap {
         final long a3_0 = _800cbe48.offset(i * 0x4L).get();
 
         v1 = -MEMORY.ref(2, t6).offset(a3_0 * 0x8L).offset(0x0L).getSigned() * x - MEMORY.ref(2, t6).offset(a3_0 * 0x8L).offset(0x4L).getSigned() * z - MEMORY.ref(4, t5).offset(a3_0 * 0xcL).offset(0x8L).get();
-        v1 = (int)v1 / MEMORY.ref(2, t6).offset(a3_0 * 0x8L).offset(0x2L).getSigned(); //TODO /0
+
+        final short divisor = (short)MEMORY.ref(2, t6).offset(a3_0 * 0x8L).offset(0x2L).getSigned();
+
+        if(divisor != 0) {
+          v1 = (int)v1 / MEMORY.ref(2, t6).offset(a3_0 * 0x8L).offset(0x2L).getSigned();
+        } else {
+          v1 = -1;
+        }
 
         v1 = v1 - (y - 0x14L);
         if((int)v1 > 0 && (int)v1 < (int)t0) {
