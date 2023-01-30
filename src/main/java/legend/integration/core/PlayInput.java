832123db532ba99efa16e0a2fbeaf51d4bbd796e
Zack Smith <zack.t.smith37@gmail.com>
2023-01-29 21:02:18 -0700

    feat: add more inputs to PlayInput

--- src/main/java/legend/integration/core/PlayInput.java
+++ src/main/java/legend/integration/core/PlayInput.java
@@ -1,25 +1,38 @@
 package legend.integration.core;
 
 import java.util.Arrays;
-import java.util.Locale;
 
 public enum PlayInput {
-    NOOP(""),
-    UP("up"),
-    DOWN("down");
+    NOOP(new String[]{""}),
+    UP(new String[]{"up"}),
+    DOWN(new String[]{"down"}),
+    LEFT(new String[]{"left"}),
+    RIGHT(new String[]{"right"}),
+    X(new String[]{"x", "cross"}),
+    SQUARE(new String[]{"square"}),
+    TRIANGLE(new String[]{"triangle"}),
+    CIRCLE(new String[]{"circle"}),
+    L1(new String[]{"l1"}),
+    L2(new String[]{"l2"}),
+    R1(new String[]{"r1"}),
+    R2(new String[]{"r2"}),
+    START(new String[]{"start"}),
+    SELECT(new String[]{"select"}),
+    ;
 
-    private String parseValue;
-    PlayInput(String parseValue) {
-        this.parseValue = parseValue;
+    private final String[] parseValues;
+
+    PlayInput(String[] parseValues) {
+        this.parseValues = parseValues;
     }
 
     public static PlayInput parse(String inputText) {
-        if(inputText == null || inputText.isBlank()) {
+        if (inputText == null || inputText.isBlank()) {
             return PlayInput.NOOP;
         }
         final String lookup = inputText.toLowerCase();
 
-        return Arrays.stream(PlayInput.values()).filter(x -> x.parseValue.equals(lookup)).findFirst().orElse(PlayInput.NOOP);
+        return Arrays.stream(PlayInput.values()).filter(pInput -> Arrays.asList(pInput.parseValues).contains(lookup)).findFirst().orElse(PlayInput.NOOP);
     }
 
 
