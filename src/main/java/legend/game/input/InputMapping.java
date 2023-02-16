e854a729e5fe2b59710d9d2792b617965ea6b793
Nick (Latch) <LatchGameDev@yahoo.com>
2023-02-16 09:31:03 -0600

    Comment block removed

--- src/main/java/legend/game/input/InputMapping.java
+++ src/main/java/legend/game/input/InputMapping.java
@@ -25,35 +25,6 @@ import static org.lwjgl.glfw.GLFW.GLFW_HAT_UP;
 public class InputMapping {
   public List<InputBinding> bindings = new ArrayList<>();
 
-  /*
-  *
-  *
-  *
-  *
-  *     final Int2IntMap gamepadKeyMap = new Int2IntOpenHashMap();
-    gamepadKeyMap.put(GLFW_KEY_SPACE, 0x100); // Select
-    gamepadKeyMap.put(GLFW_KEY_Z, 0x200); // L3
-    gamepadKeyMap.put(GLFW_KEY_C, 0x400); // R3
-    gamepadKeyMap.put(GLFW_KEY_ENTER, 0x800); // Start
-    gamepadKeyMap.put(GLFW_KEY_UP, 0x1000); // Up
-    gamepadKeyMap.put(GLFW_KEY_RIGHT, 0x2000); // Right
-    gamepadKeyMap.put(GLFW_KEY_DOWN, 0x4000); // Down
-    gamepadKeyMap.put(GLFW_KEY_LEFT, 0x8000); // Left
-    gamepadKeyMap.put(GLFW_KEY_1, 0x01); // L2
-    gamepadKeyMap.put(GLFW_KEY_3, 0x02); // R2
-    gamepadKeyMap.put(GLFW_KEY_Q, 0x04); // L1
-    gamepadKeyMap.put(GLFW_KEY_E, 0x08); // R1
-    gamepadKeyMap.put(GLFW_KEY_W, 0x10); // Triangle
-    gamepadKeyMap.put(GLFW_KEY_D, 0x40); // Circle
-    gamepadKeyMap.put(GLFW_KEY_S, 0x20); // Cross
-    gamepadKeyMap.put(GLFW_KEY_A, 0x80); // Square
-  *
-  *
-  *
-  *
-  * */
-
-
   public InputMapping() {
     this.bindings.add(new InputBinding(InputKeyCode.BUTTON_NORTH, GLFW_GAMEPAD_BUTTON_Y, 0x10, InputTypeEnum.GAMEPAD_BUTTON));
     this.bindings.add(new InputBinding(InputKeyCode.BUTTON_SOUTH, GLFW_GAMEPAD_BUTTON_A, 0x20, InputTypeEnum.GAMEPAD_BUTTON));
