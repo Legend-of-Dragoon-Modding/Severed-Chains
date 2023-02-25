40a7591ad5bb0e4869de156f5729597e021e0b2c
Andrew Mathews <flaggx@gmail.com>
2023-02-24 17:13:07 -0800

    Fixed Juint test to check for a pass or fail.

--- src/test/java/legend/game/TestHarness.java
+++ src/test/java/legend/game/TestHarness.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Assertions;
 public class TestHarness {
 
     public static void main(String[] args)  {
+
         legend.game.Main.main(args);
+        testStartOfGame();
         }
 
 
     @Test
-    public void testStartOfGame() {
-        FirstTest.startOfGame();
-        Assertions.assertTrue(true);
-
+    public static void testStartOfGame() {
+        Assertions.assertTrue(FirstTest.startOfGame());
     }
 }
