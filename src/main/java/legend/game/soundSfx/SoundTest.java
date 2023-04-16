41e9ba64ae4bba585df9388061a13ff4257f2887
Illeprih <Illeprih@gmail.com>
2023-04-16 22:26:02 +0200

    Moved play for 100Hz test

--- src/main/java/legend/game/soundSfx/SoundTest.java
+++ src/main/java/legend/game/soundSfx/SoundTest.java
@@ -7,7 +7,7 @@ import legend.core.audio.AudioThread;
 public final class SoundTest {
   public static void main(final String[] args) {
 
-    final AudioThread audioThread = new AudioThread(50, true, 24);
+    final AudioThread audioThread = new AudioThread(100, true, 24);
 
     final BackgroundMusic bgm = SoundFactory.backgroundMusic("SECT\\DRGN0.BIN\\5820");
 
