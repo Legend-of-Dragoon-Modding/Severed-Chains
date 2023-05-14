c139784f0cb3d294cdf78a929e2cb5ab8b507efa
Zychronix <zychronix41@gmail.com>
2023-05-14 19:43:22 -0400

    ...

--- src/main/java/legend/game/types/Translucency.java
+++ src/main/java/legend/game/types/Translucency.java
@@ -24,7 +24,7 @@ public enum Translucency {
   /** 0.25 x background + 0.25 x foreground */
   QUARTER_B_QUARTER_HALF_F,
   /** 0.75 x background + 0.75 x foreground */
-  TQUARTER_B_TQUARTER__F,
+  TQUARTER_B_TQUARTER_F,
   ;
 
   /** NOTE: returns null if value is -1 */
