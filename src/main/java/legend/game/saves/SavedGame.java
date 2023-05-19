891a88f760f552ef9a6b300a23511c0e269e1935
Corey Frenette <lordmonoxide@gmail.com>
2023-05-19 19:35:34 -0300

    Handle invalid saves (closes #371)

--- src/main/java/legend/game/saves/SavedGame.java
+++ src/main/java/legend/game/saves/SavedGame.java
@@ -2,11 +2,19 @@ package legend.game.saves;
 
 import legend.game.types.GameState52c;
 
+import javax.annotation.Nullable;
+
 /**
  * @param locationType 1 - world map
  *                     3 - chapter title
  *                     other - submap
  */
-public record SavedGame(String filename, int locationType, int locationIndex, GameState52c state) {
+public record SavedGame(String filename, int locationType, int locationIndex, @Nullable GameState52c state) {
+  public static SavedGame invalid(final String filename) {
+    return new SavedGame(filename, 0, 0, null);
+  }
 
+  public boolean isValid() {
+    return this.state != null;
+  }
 }
