013cb6daa72cbc7bb29977404d6085cc9335abb7
Corey Frenette <lordmonoxide@gmail.com>
2023-05-20 18:41:45 -0300

    Ensure dropdown list stays on screen (closes #540)

--- src/main/java/legend/game/inventory/screens/controls/Dropdown.java
+++ src/main/java/legend/game/inventory/screens/controls/Dropdown.java
@@ -161,9 +161,20 @@ public class Dropdown extends Control {
     private final TextRenderable[] textRenderables = new TextRenderable[Dropdown.this.options.size()];
 
     private DropdownScreen() {
-      this.addControl(Dropdown.this.panel);
-      Dropdown.this.panel.setPos(Dropdown.this.calculateTotalX() - 9, Dropdown.this.calculateTotalY() + Dropdown.this.getHeight());
-      Dropdown.this.panel.setWidth(Dropdown.this.getWidth() + 18);
+      final Panel panel = Dropdown.this.panel;
+
+      this.addControl(panel);
+      panel.setPos(Dropdown.this.calculateTotalX() - 9, Dropdown.this.calculateTotalY() + Dropdown.this.getHeight());
+
+      if(panel.getY() + panel.getHeight() > this.getHeight()) {
+        panel.setY(Dropdown.this.calculateTotalY() - panel.getHeight());
+      }
+
+      if(panel.getY() < 0) {
+        panel.setY(0);
+      }
+
+      panel.setWidth(Dropdown.this.getWidth() + 18);
 
       for(int i = 0; i < this.textRenderables.length; i++) {
         this.textRenderables[i] = TextRenderer.prepareShadowText(Dropdown.this.options.get(i), 0, 0, TextColour.BROWN);
