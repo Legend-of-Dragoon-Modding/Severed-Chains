1e0055eeba849eab2a10822c5091da3eb7c98787
Andrew Mathews <flaggx@gmail.com>
2023-06-05 20:10:17 -0700

    Updated harness with try catch block.

--- src/test/java/legend/game/TestHarness.java
+++ src/test/java/legend/game/TestHarness.java
@@ -2,18 +2,27 @@ package legend.game;
 
 import legend.core.DebugHelper;
 import legend.core.GameEngine;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 public class TestHarness {
-    @Test
+  private static final Logger LOGGER = LogManager.getFormatterLogger();
+  @Test
     public void testStartOfGame() {
         final Thread tests = new Thread(() -> {
            while(GameEngine.isLoading()) {
                DebugHelper.sleep(5);
            }
 
+          try {
             Assertions.assertTrue(FirstTest.startOfGame());
+          } catch(final AWTException e) {
+            LOGGER.error("FirstTest.startOfGame() failed to start. See stack trace.");
+          }
         });
 
         tests.setName("Tests");
