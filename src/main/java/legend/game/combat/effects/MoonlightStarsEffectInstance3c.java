35f2edf45738b6dd1eb06ef011c3aca7d87d024d
theflyingzamboni <theaerialzamboni@gmail.com>
2023-06-25 13:09:01 -0400

    Named attributes

--- src/main/java/legend/game/combat/effects/MoonlightStarsEffectInstance3c.java
+++ src/main/java/legend/game/combat/effects/MoonlightStarsEffectInstance3c.java
@@ -1,20 +1,20 @@
 package legend.game.combat.effects;
 
+import legend.core.gte.SVECTOR;
+
 public class MoonlightStarsEffectInstance3c {
   public final int index;
 
   /** short */
-  public int _00;
+  public int currentFrame_00;
 
   /** ubyte */
-  public boolean _03;
-  /** shorts */
-  public int _04;
-  public int _06;
-  public int _08;
+  public boolean renderStars_03;
+  /** 3 shorts */
+  public SVECTOR translation_04 = new SVECTOR();
 
-  public int _36;
-  public int _38;
+  public int maxFrameToggleThreshold_36;
+  public int toggleOffFrameThreshold_38;
 
   public MoonlightStarsEffectInstance3c(final int index) {
     this.index = index;
