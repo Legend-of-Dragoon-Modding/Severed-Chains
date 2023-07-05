dc39692276fc8c6c8e9bb5cb1efb2a75cff67380
theflyingzamboni <theaerialzamboni@gmail.com>
2023-07-04 22:50:25 -0400

    Fixed signedness issue in ParticleInnerStuff04

--- src/main/java/legend/game/combat/effects/ParticleInnerStuff04.java
+++ src/main/java/legend/game/combat/effects/ParticleInnerStuff04.java
@@ -3,21 +3,22 @@ package legend.game.combat.effects;
 import legend.core.memory.Value;
 import legend.core.memory.types.ByteRef;
 import legend.core.memory.types.MemoryRef;
+import legend.core.memory.types.UnsignedByteRef;
 
 public class ParticleInnerStuff04 implements MemoryRef {
   private final Value ref;
 
-  public final ByteRef renderFrameCount_00;
-  public final ByteRef colour_01;
-  public final ByteRef _02;
+  public final UnsignedByteRef renderFrameCount_00;
+  public final UnsignedByteRef colour_01;
+  public final UnsignedByteRef _02;
   public final ByteRef flag_03;
 
   public ParticleInnerStuff04(final Value ref) {
     this.ref = ref;
 
-    this.renderFrameCount_00 = ref.offset(1, 0x00).cast(ByteRef::new);
-    this.colour_01 = ref.offset(1, 0x01).cast(ByteRef::new);
-    this._02 = ref.offset(1, 0x02).cast(ByteRef::new);
+    this.renderFrameCount_00 = ref.offset(1, 0x00).cast(UnsignedByteRef::new);
+    this.colour_01 = ref.offset(1, 0x01).cast(UnsignedByteRef::new);
+    this._02 = ref.offset(1, 0x02).cast(UnsignedByteRef::new);
     this.flag_03 = ref.offset(1, 0x03).cast(ByteRef::new);
   }
 
