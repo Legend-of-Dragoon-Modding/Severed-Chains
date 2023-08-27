c5497cb4e7089c4e34868dbeae0038fe3618ddbc
theflyingzamboni <theaerialzamboni@gmail.com>
2023-08-26 23:54:03 -0400

    Fixed Bttl lighting bug

--- src/main/java/legend/game/combat/environment/BttlLightStruct84Sub38.java
+++ src/main/java/legend/game/combat/environment/BttlLightStruct84Sub38.java
@@ -4,7 +4,7 @@ import org.joml.Vector3f;
 
 public class BttlLightStruct84Sub38 {
   public int _00;
-  public final Vector3f angle_04 = new Vector3f();
+  public final Vector3f angleAndColour_04 = new Vector3f();
   public final Vector3f vec_10 = new Vector3f();
   public final Vector3f vec_1c = new Vector3f();
   public final Vector3f vec_28 = new Vector3f();
@@ -12,7 +12,7 @@ public class BttlLightStruct84Sub38 {
 
   public void clear() {
     this._00 = 0;
-    this.angle_04.zero();
+    this.angleAndColour_04.zero();
     this.vec_10.zero();
     this.vec_1c.zero();
     this.vec_28.zero();
