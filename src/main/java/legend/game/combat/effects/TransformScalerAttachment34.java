2ee5f100229f4aebdc2482c6e214256d6e9a9f5a
Corey Frenette <lordmonoxide@gmail.com>
2023-09-28 17:31:27 -0300

    Fix  most effect issues

--- src/main/java/legend/game/combat/effects/TransformScalerAttachment34.java
+++ src/main/java/legend/game/combat/effects/TransformScalerAttachment34.java
@@ -4,11 +4,11 @@ import legend.game.combat.types.BattleObject;
 import org.joml.Vector3f;
 
 public class TransformScalerAttachment34 extends EffectAttachment {
-  /** Was 12-bit fixed-point */
+  /** Was 8-bit fixed-point */
   public final Vector3f value_0c = new Vector3f();
-  /** Was 12-bit fixed-point */
+  /** Was 8-bit fixed-point */
   public final Vector3f velocity_18 = new Vector3f();
-  /** Was 12-bit fixed-point */
+  /** Was 8-bit fixed-point */
   public final Vector3f acceleration_24 = new Vector3f();
   public BattleObject parent_30;
   public int ticksRemaining_32;
