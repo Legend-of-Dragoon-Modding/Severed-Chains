3fec4552b4dfc12ef6333a3195baa77b7303cab1
theflyingzamboni <theaerialzamboni@gmail.com>
2023-11-12 23:51:29 -0500

    Fixed wmap smoke bug

--- src/main/java/legend/game/wmap/WmapSmokeInstance60.java
+++ src/main/java/legend/game/wmap/WmapSmokeInstance60.java
@@ -3,7 +3,7 @@ package legend.game.wmap;
 import legend.core.gte.GsCOORDINATE2;
 import org.joml.Vector3f;
 
-public class WmapSmokeCloudInstance60 {
+public class WmapSmokeInstance60 {
   public final GsCOORDINATE2 coord2_00 = new GsCOORDINATE2();
   public float scaleAndColourFade_50;
   public final Vector3f translationOffset_54 = new Vector3f();
