65f8b9272620f2ecd9a608d42aacfcf77930f1a5
Corey Frenette <lordmonoxide@gmail.com>
2024-01-14 12:48:36 -0400

    Add foreground count to SubmapEnvironmentTextureEvent

--- src/main/java/legend/game/modding/events/submap/SubmapEnvironmentTextureEvent.java
+++ src/main/java/legend/game/modding/events/submap/SubmapEnvironmentTextureEvent.java
@@ -5,11 +5,13 @@ import legend.core.opengl.Texture;
 public class SubmapEnvironmentTextureEvent extends SubmapEvent {
   public final int disk;
   public final int submapCut;
+  public final int foregroundCount;
   public Texture background;
   public Texture[] foregrounds;
 
-  public SubmapEnvironmentTextureEvent(final int disk, final int submapCut) {
+  public SubmapEnvironmentTextureEvent(final int disk, final int submapCut, final int foregroundCount) {
     this.disk = disk;
     this.submapCut = submapCut;
+    this.foregroundCount = foregroundCount;
   }
 }
