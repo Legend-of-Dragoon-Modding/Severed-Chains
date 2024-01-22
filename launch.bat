87f331fba1275d5862ee3ec8cee2efa0fc62f0e5
Corey Frenette <lordmonoxide@gmail.com>
2024-01-22 17:57:13 -0400

    Update to Java 21 and Gradle 8.5

--- launch.bat
+++ launch.bat
@@ -1,23 +1,23 @@
 @echo off
 
-if exist ".\jdk\bin\java.exe" (
+if exist ".\jdk21\bin\java.exe" (
   goto LAUNCH
 )
 
 : DOWNLOAD_JAVA
-if exist ".\jdk\" (
-  @rmdir /S /Q ".\jdk"
+if exist ".\jdk21\" (
+  @rmdir /S /Q ".\jdk21"
 )
 
 echo Downloading java...
-powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://corretto.aws/downloads/resources/17.0.7.7.1/amazon-corretto-17.0.7.7.1-windows-x64-jdk.zip' -OutFile '.\jdk.zip'"
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://corretto.aws/downloads/resources/21.0.2.13.1/amazon-corretto-21.0.2.13.1-windows-x64-jdk.zip' -OutFile '.\jdk.zip'"
 
 echo Extracting java...
 powershell Expand-Archive ".\jdk.zip" -DestinationPath "."
 
 echo Cleaning up...
-move ".\jdk17.0.7_7" ".\jdk"
+move ".\jdk21.0.2_13" ".\jdk21"
 del ".\jdk.zip"
 
 : LAUNCH
-".\jdk\bin\java" -cp "lod-game-@version@.jar;libs/*" legend.game.MainWindows -Xmx2G -ea -Djoml.fastmath -Djoml.sinLookup -Djoml.useMathFma || pause
+".\jdk21\bin\java" -cp "lod-game-@version@.jar;libs/*" legend.game.MainWindows -Xmx2G -ea -Djoml.fastmath -Djoml.sinLookup -Djoml.useMathFma || pause
