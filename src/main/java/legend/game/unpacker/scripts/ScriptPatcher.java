200e6028ddbe14f303d500e10e7e89f03d6f4ab5
icarus <avionanx@gmail.com>
2024-05-12 04:02:50 +0300

    update decompiler

--- src/main/java/legend/game/unpacker/scripts/ScriptPatcher.java
+++ src/main/java/legend/game/unpacker/scripts/ScriptPatcher.java
@@ -135,7 +135,9 @@ public class ScriptPatcher {
   }
 
   private List<String> decompile(final byte[] data) {
-    final Script script = this.disassembler.disassemble(data);
+    //TODO
+    int[] arr = {};
+    final Script script = this.disassembler.disassemble(data,arr);
     final String decompiledOutput = this.translator.translate(script, this.meta);
     return decompiledOutput.lines().toList();
   }
