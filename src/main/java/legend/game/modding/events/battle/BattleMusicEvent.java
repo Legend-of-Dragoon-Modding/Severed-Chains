b2046e3fc7e8a7fe8a7e2dad544f3e37046d114e
Ink230 <justinquinn230@gmail.com>
2024-12-20 06:53:47 -0500

    fix mod events using final on moddable properties

--- src/main/java/legend/game/modding/events/battle/BattleMusicEvent.java
+++ src/main/java/legend/game/modding/events/battle/BattleMusicEvent.java
@@ -4,8 +4,8 @@ import legend.game.combat.bent.BattleEvent;
 import legend.game.combat.environment.StageData2c;
 
 public class BattleMusicEvent extends BattleEvent {
-  public final int victoryType;
-  public final int musicIndex;
+  public int victoryType;
+  public int musicIndex;
   public final StageData2c stageData;
 
   public BattleMusicEvent(final int victoryType, final int musicIndex, final StageData2c stageData) {
