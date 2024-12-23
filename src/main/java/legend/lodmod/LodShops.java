1acf988d419f71453e3a44a8921e7862454932dc
Ink230 <justinquinn230@gmail.com>
2024-12-23 14:19:26 -0500

    add shop registry to lodmod and SMap

---
+++ src/main/java/legend/lodmod/LodShops.java
@@ -0,0 +1,50 @@
+package legend.lodmod;
+
+import legend.core.GameEngine;
+import legend.game.types.ShopStruct40;
+import org.legendofdragoon.modloader.registries.RegistryDelegate;
+
+public class LodShops {
+  private LodShops() { }
+
+  public static final RegistryDelegate<ShopStruct40> BALE_EQUIPMENT_CASTLE = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("bale_equipment_castle"));
+  public static final RegistryDelegate<ShopStruct40> SERDIO_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("serdio_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> LOHAN_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("lohan_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> LOHAN_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("lohan_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> KAZAS_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("kazas_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> KAZAS_FORT_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("kazas_fort_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> FLETZ_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("fletz_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> FLETZ_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("fletz_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> DONAU_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("donau_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> DONAU_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("donau_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> QUEEN_FURY_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("queen_fury_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> QUEEN_FURY_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("queen_fury_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> FUENO_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("fueno_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> FUENO_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("fueno_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> FURNI_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("furni_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> FURNI_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("furni_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> DENINGRAD_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("deningrad_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> DENINGRAD_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("deningrad_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> WINGLY_FOREST_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("wingly_forest_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> WINGLY_FOREST_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("wingly_forest_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> VELLWEB_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("vellweb_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> VELLWEB_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("vellweb_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> ULARA_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("ulara_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> ULARA_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("ulara_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> ROUGE_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("rouge_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> ROUGE_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("rouge_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> MOON_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("moon_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> MOON_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("moon_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> HELLENA_01_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("hellena_01_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> KASHUA_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("kashua_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> KASHUA_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("kashua_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> FLETZ_ACCESSORY_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("fletz_accessory_shop"));
+  public static final RegistryDelegate<ShopStruct40> FOREST_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("forest_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> KAZAS_FORT_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("kazas_fort_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> VOLCANO_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("volcano_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> ZENEBATOS_EQUIPMENT_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("zenebatos_equipment_shop"));
+  public static final RegistryDelegate<ShopStruct40> ZENEBATOS_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("zenebatos_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> HELLENA_02_ITEM_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("hellena_02_item_shop"));
+  public static final RegistryDelegate<ShopStruct40> UNKNOWN_SHOP_01 = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("unknown_shop_01"));
+  public static final RegistryDelegate<ShopStruct40> EMPTY_SHOP = GameEngine.REGISTRIES.shop.getEntry(LodMod.id("empty_shop"));
+}
