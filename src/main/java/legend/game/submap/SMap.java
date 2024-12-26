6374665d9c7ed7fa7271ee7cd984238b70961d4b
warblgarbl <136350269+warblgarbl@users.noreply.github.com>
2024-12-26 01:55:50 -0500

    Remove extra arrows on Phantom Ship chest code

--- src/main/java/legend/game/submap/SMap.java
+++ src/main/java/legend/game/submap/SMap.java
@@ -652,7 +652,10 @@ public class SMap extends EngineState {
     }
 
     //LAB_800253d4
-    textboxText.flags_08 |= TextboxText84.SHOW_ARROW;
+    /* This is not a retail flag. Used in a script patch to remove arrows from 3 of 4 overlapping textboxes for Phantom Ship's code-locked chest. */
+    if((script.params_20[1].get() & 0x2000) > 0) {
+      script.params_20[1].set(script.params_20[1].get() ^ 0x2000);
+    } else textboxText.flags_08 |= TextboxText84.SHOW_ARROW;
     textboxText.chars_58 = new TextboxChar08[textboxText.chars_1c * (textboxText.lines_1e + 1)];
     Arrays.setAll(textboxText.chars_58, i -> new TextboxChar08());
     this.positionSobjTextbox(textboxIndex, script.params_20[1].get());
