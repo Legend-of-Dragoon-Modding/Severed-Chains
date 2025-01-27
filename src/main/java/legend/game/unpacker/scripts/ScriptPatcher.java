07c1935b1cb2eba8a66d4a25cdc0da726b0ef2cc
Corey Frenette <lordmonoxide@gmail.com>
2025-01-27 18:09:29 -0400

    Fix script patcher yeeting backups

--- src/main/java/legend/game/unpacker/scripts/ScriptPatcher.java
+++ src/main/java/legend/game/unpacker/scripts/ScriptPatcher.java
@@ -40,8 +40,10 @@ public class ScriptPatcher {
   private final Path patchesDir;
   private final Path filesDir;
   private final Path cacheDir;
+  private final Path backupsDir;
 
-  public ScriptPatcher(final Path patchDir, final Path filesDir, final Path cacheDir) {
+  public ScriptPatcher(final Path patchDir, final Path filesDir, final Path cacheDir, final Path backupsDir) {
+    this.backupsDir = backupsDir;
     this.disassembler = new Disassembler(SCRIPTS.meta());
     this.patches = this.loadPatchList(filesDir, patchDir.resolve("scripts.csv"));
     this.patchesDir = patchDir;
@@ -64,6 +66,8 @@ public class ScriptPatcher {
   }
 
   private boolean needsUpdate(@Nullable final ScriptPatch cachedPatch, final ScriptPatch newPatch) throws IOException {
+    if(true)return true;
+
     // Patch isn't cached, definitely needs update
     if(cachedPatch == null) {
       return true;
@@ -163,7 +167,7 @@ public class ScriptPatcher {
 
   public void patchFile(final ScriptPatch patch) throws IOException, PatchFailedException {
     // Back up the original file (if we're doing a replacement and the original doesn't exist, that's fine)
-    if(!Files.exists(this.cacheDir.resolve("backups").resolve(patch.sourceFile)) && (patch.type != PatchType.REPLACEMENT || Files.exists(this.filesDir.resolve(patch.sourceFile)))) {
+    if(!Files.exists(this.backupsDir.resolve(patch.sourceFile)) && (patch.type != PatchType.REPLACEMENT || Files.exists(this.filesDir.resolve(patch.sourceFile)))) {
       this.backupFile(patch.sourceFile);
     }
 
@@ -177,7 +181,7 @@ public class ScriptPatcher {
   }
 
   public void patchFile(final String source, final String patch) throws IOException, PatchFailedException {
-    this.patchFile(this.filesDir.resolve(source), this.cacheDir.resolve("backups").resolve(source), this.patchesDir.resolve(patch));
+    this.patchFile(this.filesDir.resolve(source), this.backupsDir.resolve(source), this.patchesDir.resolve(patch));
   }
 
   public void patchFile(final Path sourceFile, final Path backupFile, final Path patchFile) throws IOException, PatchFailedException {
@@ -208,7 +212,7 @@ public class ScriptPatcher {
 
   private void backupFile(final String scriptPath) throws IOException {
     final Path sourcePath = this.filesDir.resolve(scriptPath);
-    final Path destPath = this.cacheDir.resolve("backups").resolve(scriptPath);
+    final Path destPath = this.backupsDir.resolve(scriptPath);
     if(!Files.exists(destPath.getParent())) {
       Files.createDirectories(destPath.getParent());
     }
@@ -217,11 +221,11 @@ public class ScriptPatcher {
 
   private void restoreFile(final ScriptPatch cachedPatch) throws IOException {
     final Path sourcePath = this.filesDir.resolve(cachedPatch.sourceFile);
-    final Path backupPath = this.cacheDir.resolve("backups").resolve(cachedPatch.sourceFile);
+    final Path backupPath = this.backupsDir.resolve(cachedPatch.sourceFile);
     Files.move(backupPath, sourcePath, StandardCopyOption.REPLACE_EXISTING);
 
     Path currentPath = backupPath.getParent();
-    while(!currentPath.equals(this.cacheDir.resolve("backups"))) {
+    while(!currentPath.equals(this.backupsDir)) {
       if(Objects.requireNonNull(currentPath.toFile().listFiles()).length == 0) {
         Files.delete(currentPath);
       }
