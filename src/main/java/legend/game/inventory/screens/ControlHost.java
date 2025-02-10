46cf782bde6bcb812d9a3abfcfea3984b164b70b
Francis Dionne <96087943+FrancisDionne@users.noreply.github.com>
2025-02-09 18:00:18 -0800

    Implemented try catches on every OptionsScreen user handles to handle errors thrown by configs

--- src/main/java/legend/game/inventory/screens/ControlHost.java
+++ src/main/java/legend/game/inventory/screens/ControlHost.java
@@ -84,6 +84,10 @@ public abstract class ControlHost implements Iterable<Control> {
     return this.controls.iterator();
   }
 
+  protected Control findControlAt(final int index) {
+    return index > -1 && index < this.controls.size() ? this.controls.get(index) : null;
+  }
+
   protected Control findControlAt(final int x, final int y) {
     return this.findControlAt(x, y, false);
   }
--- src/main/java/legend/game/inventory/screens/OptionsScreen.java
+++ src/main/java/legend/game/inventory/screens/OptionsScreen.java
@@ -1,6 +1,7 @@
 package legend.game.inventory.screens;
 
 import legend.core.GameEngine;
+import legend.core.RenderEngine;
 import legend.game.i18n.I18n;
 import legend.game.input.InputAction;
 import legend.game.inventory.screens.controls.Background;
@@ -9,8 +10,11 @@ import legend.game.saves.ConfigCategory;
 import legend.game.saves.ConfigCollection;
 import legend.game.saves.ConfigEntry;
 import legend.game.saves.ConfigStorageLocation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.legendofdragoon.modloader.registries.RegistryId;
 
+import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -22,6 +26,7 @@ import static legend.game.Scus94491BpeSegment_8002.renderText;
 import static legend.game.Scus94491BpeSegment_8002.textWidth;
 
 public class OptionsScreen extends VerticalLayoutScreen {
+  private static final Logger LOGGER = LogManager.getFormatterLogger(OptionsScreen.class);
   private final Runnable unload;
 
   private final Map<Control, Label> helpLabels = new HashMap<>();
@@ -56,10 +61,23 @@ public class OptionsScreen extends VerticalLayoutScreen {
 
         if(validLocations.contains(configEntry.storageLocation) && configEntry.hasEditControl()) {
           //noinspection unchecked
-          final Control editControl = configEntry.makeEditControl(config.getConfig(configEntry), config);
+          Control editControl;
+          boolean error = false;
+
+          try {
+            editControl = configEntry.makeEditControl(config.getConfig(configEntry), config);
+          } catch(final Throwable ex){
+            editControl = this.createErrorLabel("Error creating control", ex, false);
+            error = true;
+          }
+
           editControl.setZ(35);
 
           final Label label = this.addRow(text, editControl);
+
+          if(error) {
+            label.getFontOptions().colour(0.30f, 0.0f, 0.0f).shadowColour(TextColour.LIGHT_BROWN);
+          }
 
           if(configEntry.hasHelp()) {
             final Label help = label.addControl(new Label("?"));
@@ -73,27 +91,57 @@ public class OptionsScreen extends VerticalLayoutScreen {
       });
   }
 
+  private Label createErrorLabel(final String log, final Object obj, final boolean setSize) {
+    LOGGER.warn(log, obj);
+    final Label l = new Label("Error");
+    l.getFontOptions().colour(0.30f, 0.0f, 0.0f).shadowColour(TextColour.LIGHT_BROWN);
+
+    if(setSize) {
+      l.setSize(140, 11);
+      l.setPos(this.getWidth() - 64 - l.getWidth(), 0);
+      l.setScale(0.66f);
+    }
+
+    return l;
+  }
+
+  private void replaceControlWithErrorLabel(final String log, final Object obj) {
+    final Label row = this.getHighlightedRow();
+    if(row != null) {
+      row.getFontOptions().colour(0.30f, 0.0f, 0.0f).shadowColour(TextColour.LIGHT_BROWN);
+      final Control c = row.findControlAt(0);
+      if(c != null) {
+        row.removeControl(c);
+        row.addControl(this.createErrorLabel(log, obj, true));
+      }
+    }
+  }
+
   @Override
   public InputPropagation pressedThisFrame(final InputAction inputAction) {
-    if(super.pressedThisFrame(inputAction) == InputPropagation.HANDLED) {
-      return InputPropagation.HANDLED;
-    }
-
-    if(inputAction == InputAction.BUTTON_EAST) {
-      playMenuSound(3);
-      this.unload.run();
-      return InputPropagation.HANDLED;
-    }
-
-    if(inputAction == InputAction.BUTTON_NORTH) {
-      final ConfigEntry<?> configEntry = this.helpEntries.get(this.getHighlightedRow());
-      if(configEntry != null) {
-        playMenuSound(1);
-        final Label helpLabel = this.helpLabels.get(this.getHighlightedRow());
-        this.getStack().pushScreen(new TooltipScreen(I18n.translate(configEntry.getHelpTranslationKey()), helpLabel.calculateTotalX() + helpLabel.getWidth() / 2, helpLabel.calculateTotalY() + helpLabel.getHeight() / 2));
+    try {
+      if(super.pressedThisFrame(inputAction) == InputPropagation.HANDLED) {
+        return InputPropagation.HANDLED;
       }
 
-      return InputPropagation.HANDLED;
+      if(inputAction == InputAction.BUTTON_EAST) {
+        playMenuSound(3);
+        this.unload.run();
+        return InputPropagation.HANDLED;
+      }
+
+      if(inputAction == InputAction.BUTTON_NORTH) {
+        final ConfigEntry<?> configEntry = this.helpEntries.get(this.getHighlightedRow());
+        if(configEntry != null) {
+          playMenuSound(1);
+          final Label helpLabel = this.helpLabels.get(this.getHighlightedRow());
+          this.getStack().pushScreen(new TooltipScreen(I18n.translate(configEntry.getHelpTranslationKey()), helpLabel.calculateTotalX() + helpLabel.getWidth() / 2, helpLabel.calculateTotalY() + helpLabel.getHeight() / 2));
+        }
+
+        return InputPropagation.HANDLED;
+      }
+    } catch(final Throwable ex) {
+      this.replaceControlWithErrorLabel("Error on pressedThisFrame", ex);
     }
 
     return InputPropagation.PROPAGATE;
@@ -101,7 +149,90 @@ public class OptionsScreen extends VerticalLayoutScreen {
 
   @Override
   protected void render() {
-    super.render();
-    renderText(I18n.translate("lod_core.ui.options.help_hotkey", "\u0120"), 334, 226, this.fontOptions);
+    try {
+      super.render();
+      renderText(I18n.translate("lod_core.ui.options.help_hotkey", "\u0120"), 334, 226, this.fontOptions);
+    } catch(final Throwable ex) {
+      this.replaceControlWithErrorLabel("Error on render", ex);
+    }
+  }
+
+  @Override
+  protected InputPropagation mouseMove(final int x, final int y) {
+    try {
+      return super.mouseMove(x, y);
+    } catch(final Throwable ex) {
+      this.replaceControlWithErrorLabel("Error on keyPress", ex);
+    }
+    return InputPropagation.PROPAGATE;
+  }
+
+  @Override
+  protected InputPropagation mouseScroll(final int deltaX, final int deltaY) {
+    try {
+      return super.mouseScroll(deltaX, deltaY);
+    } catch(final Throwable ex) {
+      this.replaceControlWithErrorLabel("Error on mouseScroll", ex);
+    }
+    return InputPropagation.PROPAGATE;
+  }
+
+  @Override
+  protected InputPropagation pressedWithRepeatPulse(final InputAction inputAction) {
+    try {
+      return super.pressedWithRepeatPulse(inputAction);
+    } catch(final Throwable ex) {
+      this.replaceControlWithErrorLabel("Error on pressedWithRepeatPulse", ex);
+    }
+    return InputPropagation.PROPAGATE;
+  }
+
+  @Override
+  protected InputPropagation keyPress(final int key, final int scancode, final int mods) {
+    try {
+      return super.keyPress(key, scancode, mods);
+    } catch(final Throwable ex) {
+      this.replaceControlWithErrorLabel("Error on keyPress", ex);
+    }
+    return InputPropagation.PROPAGATE;
+  }
+
+  @Override
+  protected InputPropagation charPress(final int codepoint) {
+    try {
+      return super.charPress(codepoint);
+    } catch(final Throwable ex) {
+      this.replaceControlWithErrorLabel("Error on charPress", ex);
+    }
+    return InputPropagation.PROPAGATE;
+  }
+
+  @Override
+  protected InputPropagation releasedThisFrame(final InputAction inputAction) {
+    try {
+      return super.releasedThisFrame(inputAction);
+    } catch(final Throwable ex) {
+      this.replaceControlWithErrorLabel("Error on releasedThisFrame", ex);
+    }
+    return InputPropagation.PROPAGATE;
+  }
+
+  @Override
+  public void setFocus(@Nullable final Control control) {
+    try {
+      super.setFocus(control);
+    } catch(final Throwable ex) {
+      this.replaceControlWithErrorLabel("Error on setFocus", ex);
+    }
+  }
+
+  @Override
+  protected InputPropagation mouseScrollHighRes(final double deltaX, final double deltaY) {
+    try {
+      return super.mouseScrollHighRes(deltaX, deltaY);
+    } catch(final Throwable ex) {
+      this.replaceControlWithErrorLabel("Error on mouseScrollHighRes", ex);
+    }
+    return InputPropagation.PROPAGATE;
   }
 }
