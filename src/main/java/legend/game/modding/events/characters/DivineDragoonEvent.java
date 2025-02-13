cbd66a465b5c24999825a62180bff899941161ef
Zychronix <zychronix41@gmail.com>
2025-02-12 19:36:34 -0500

    Comment update

--- src/main/java/legend/game/modding/events/characters/DivineDragoonEvent.java
+++ src/main/java/legend/game/modding/events/characters/DivineDragoonEvent.java
@@ -3,7 +3,7 @@ package legend.game.modding.events.characters;
 import org.legendofdragoon.modloader.events.Event;
 
 /**
- * Fired any time a goods is checked for Divine Dragon Spirit or initially getting Divine Dragoon
+ * Fired before the game auto levels Dart's dragoon level and exp when acquiring the Divine Dragoon Spirit.
  */
 public class DivineDragoonEvent extends Event {
   /** Setting this to true will prevent the base game from auto leveling Dart's Dragoon EXP and Level to a set amount. */
