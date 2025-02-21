279a61512daf7ab5c1ca29296dc03ad7d4de7872
Corey Frenette <lordmonoxide@gmail.com>
2025-02-21 17:37:57 -0400

    Update jdk/gradle/libs (closes #2115)

--- launch.bat
+++ launch.bat
@@ -18,14 +18,19 @@ if exist ".\jdk21\" (
 )
 
 echo Downloading java...
-powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://corretto.aws/downloads/resources/21.0.2.13.1/amazon-corretto-21.0.2.13.1-windows-x64-jdk.zip' -OutFile '.\jdk.zip'"
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://corretto.aws/downloads/resources/21.0.6.7.1/amazon-corretto-21.0.6.7.1-windows-x64-jdk.zip' -OutFile '.\jdk.zip'" || goto DOWNLOAD_FAILED
 
 echo Extracting java...
-powershell Expand-Archive ".\jdk.zip" -DestinationPath "."
+powershell Expand-Archive ".\jdk.zip" -DestinationPath "." || goto DOWNLOAD_FAILED
 
 echo Cleaning up...
-move ".\jdk21.0.2_13" ".\jdk21"
+move ".\jdk21.0.6_7" ".\jdk21"
 del ".\jdk.zip"
 
 : LAUNCH
 ".\jdk21\bin\java" -Djoml.fastmath -Djoml.sinLookup -Djoml.useMathFma -cp "lod-game-@version@.jar;@libs@" legend.game.MainWindows -Xmx2G -ea || pause
+exit 0
+
+: DOWNLOAD_FAILED
+echo Java download failed
+exit 2
