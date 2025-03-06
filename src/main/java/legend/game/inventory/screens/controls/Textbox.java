a8c58c955438f7013d0d143ff366305635ea1e04
Corey Frenette <lordmonoxide@gmail.com>
2025-03-05 20:47:04 -0400

    Update textbox input for menu confirm/back

--- src/main/java/legend/game/inventory/screens/controls/Textbox.java
+++ src/main/java/legend/game/inventory/screens/controls/Textbox.java
@@ -19,6 +19,8 @@ import static legend.game.Scus94491BpeSegment_8002.renderText;
 import static legend.game.Scus94491BpeSegment_8002.textHeight;
 import static legend.game.Scus94491BpeSegment_8002.textWidth;
 import static legend.game.Scus94491BpeSegment_800b.textZ_800bdf00;
+import static legend.game.modding.coremod.CoreMod.INPUT_ACTION_MENU_BACK;
+import static legend.game.modding.coremod.CoreMod.INPUT_ACTION_MENU_CONFIRM;
 
 public class Textbox extends Control {
   private final Panel background;
@@ -175,10 +177,6 @@ public class Textbox extends Control {
       this.fireChangedEvent();
     }
 
-    if(key == InputKey.ESCAPE || key == InputKey.RETURN) {
-      this.deferAction(this::unfocus);
-    }
-
     return InputPropagation.HANDLED;
   }
 
@@ -203,6 +201,10 @@ public class Textbox extends Control {
 
   @Override
   protected InputPropagation inputActionPressed(final InputAction action, final boolean repeat) {
+    if((action == INPUT_ACTION_MENU_CONFIRM.get() || action == INPUT_ACTION_MENU_BACK.get()) && !repeat) {
+      this.deferAction(this::unfocus);
+    }
+
     return InputPropagation.HANDLED;
   }
 
