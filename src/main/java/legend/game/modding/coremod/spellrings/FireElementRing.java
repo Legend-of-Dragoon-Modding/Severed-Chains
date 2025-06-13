Grey-Mad/LoD_SC_party_uncapped

moddable spell ring colours

src/main/java/legend/game/modding/coremod/spellrings/FireElementRing.java
@@ -1,0 +1,7 @@
+package legend.game.modding.coremod.spellrings;
+
+public class FireElementRing extends SimpleElementRing {
+  public FireElementRing() {
+    super(0x6c8283);
+  }
+}

Legend-of-Dragoon-Modding/Severed-Chains

src/main/java/legend/game/modding/coremod/spellrings/FireElementRing.java
@@ -1,0 +1,7 @@
+package legend.game.modding.coremod.spellrings;
+
+public class FireElementRing extends SimpleElementRing {
+  public FireElementRing() {
+    super(0x201996);
+  }
+}