ce588fb218fdb5e2dbb89a8dbb18964b3ea3bb59
Corey Frenette <lordmonoxide@gmail.com>
2025-10-12 10:15:31 -0300

    Remove stray debug line

--- src/main/java/legend/game/Scus94491BpeSegment.java
+++ src/main/java/legend/game/Scus94491BpeSegment.java
@@ -679,7 +679,6 @@ public final class Scus94491BpeSegment {
 
     fullScreenEffect_800bb140._24 = _8004dd48[effectType];
     fullScreenEffect_800bb140.currentColour_28 = 1;
-    System.out.println("set");
 
     //LAB_80013768
   }
