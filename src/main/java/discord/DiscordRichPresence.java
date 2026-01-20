d9dd56bcaa2134eaae2131423e3c3e91a3a42c97
Corey Frenette <lordmonoxide@gmail.com>
2026-01-20 11:25:44 -0400

    Thread discord init (closes #2538)

--- src/main/java/discord/DiscordRichPresence.java
+++ src/main/java/discord/DiscordRichPresence.java
@@ -5,10 +5,12 @@ import de.jcm.discordgamesdk.CreateParams;
 import de.jcm.discordgamesdk.GameSDKException;
 import de.jcm.discordgamesdk.LogLevel;
 import de.jcm.discordgamesdk.activity.Activity;
+import legend.core.Async;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.time.Instant;
+import java.util.concurrent.TimeUnit;
 
 public final class DiscordRichPresence {
   private static final Logger LOGGER = LogManager.getFormatterLogger(DiscordRichPresence.class);
@@ -23,16 +25,20 @@ public final class DiscordRichPresence {
   }
 
   public void tick() {
-    if(this.core != null) {
-      try {
-        this.core.runCallbacks();
-      } catch(final GameSDKException ignored) { }
+    synchronized(this) {
+      if(this.core != null) {
+        try {
+          this.core.runCallbacks();
+        } catch(final GameSDKException ignored) { }
+      }
     }
   }
 
   public void updateActivity() {
-    if(this.core != null) {
-      this.core.activityManager().updateActivity(this.activity);
+    synchronized(this) {
+      if(this.core != null) {
+        this.core.activityManager().updateActivity(this.activity);
+      }
     }
   }
 
@@ -43,23 +49,31 @@ public final class DiscordRichPresence {
     final long paramFlags = CreateParams.Flags.toLong(CreateParams.Flags.NO_REQUIRE_DISCORD, CreateParams.Flags.SUPPRESS_EXCEPTIONS);
     params.setFlags(paramFlags);
 
-    try {
-      this.core = new Core(params);
-    } catch(final Exception e) {
-      LOGGER.warn("Failed to initialize Discord rich presence", e);
-      return;
-    }
+    Async.run(() -> {
+      final Core core = new Core(params);
+      core.setLogHook(LogLevel.ERROR, (level, string) -> LOGGER.error(string));
+      core.setLogHook(LogLevel.WARN, (level, string) -> LOGGER.warn(string));
+      core.setLogHook(LogLevel.INFO, (level, string) -> LOGGER.info(string));
+      core.setLogHook(LogLevel.DEBUG, (level, string) -> LOGGER.debug(string));
+      core.setLogHook(LogLevel.VERBOSE, (level, string) -> LOGGER.trace(string));
 
-    this.core.setLogHook(LogLevel.ERROR, (level, string) -> LOGGER.error(string));
-    this.core.setLogHook(LogLevel.WARN, (level, string) -> LOGGER.warn(string));
-    this.core.setLogHook(LogLevel.INFO, (level, string) -> LOGGER.info(string));
-    this.core.setLogHook(LogLevel.DEBUG, (level, string) -> LOGGER.debug(string));
-    this.core.setLogHook(LogLevel.VERBOSE, (level, string) -> LOGGER.trace(string));
+      synchronized(this) {
+        this.core = core;
+      }
+    })
+      .orTimeout(10, TimeUnit.SECONDS)
+      .exceptionally(t -> {
+        LOGGER.warn("Failed to initialize Discord rich presence", t);
+        return null;
+      });
   }
 
   public void destroy() {
-    if(this.core != null) {
-      this.core.close();
+    synchronized(this) {
+      if(this.core != null) {
+        this.core.close();
+        this.core = null;
+      }
     }
   }
 }
