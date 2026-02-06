7704f140667c0d3302567e56fec48890647b4e8e
Corey Frenette <lordmonoxide@gmail.com>
2026-02-06 08:05:06 -0400

    Fix auto-addition check

--- src/main/java/legend/game/combat/types/battlestate/AdditionExtra04.java
+++ src/main/java/legend/game/combat/types/battlestate/AdditionExtra04.java
@@ -31,7 +31,7 @@ public class AdditionExtra04 {
     int ultimateWargod = 0;
     // This is a cheap way to tell if we're in a combat engine cutscene and turn it off
     if(CONFIG.getConfig(CoreMod.ADDITION_MODE_CONFIG.get()) == AdditionMode.AUTOMATIC) {
-      final ScriptState<? extends BattleEntity27c> combatant = battleState_8006e398.allBents_e0c.get(this.index);
+      final ScriptState<? extends BattleEntity27c> combatant = this.index < battleState_8006e398.allBents_e0c.size() ? battleState_8006e398.allBents_e0c.get(this.index) : null;
 
       if(combatant != null && !combatant.hasFlag(FLAG_MONSTER)) {
         boolean enemyAlive = false;
