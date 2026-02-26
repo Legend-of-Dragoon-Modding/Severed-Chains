0be5a522220480096537c57301b88a082da3cbee
illeprih <illeprih@gmail.com>
2026-02-26 21:35:13 +0100

    Base for SFX loading

--- src/main/java/legend/core/audio/SfxTest.java
+++ src/main/java/legend/core/audio/SfxTest.java
@@ -1,7 +1,5 @@
 package legend.core.audio;
 
-import legend.core.DebugHelper;
-import legend.core.audio.sequencer.assets.BackgroundMusic;
 import legend.core.audio.sequencer.assets.Sfx;
 import legend.game.unpacker.Loader;
 
@@ -9,7 +7,7 @@ public final class SfxTest {
 
   private SfxTest() {}
 
-  public static void main(final String[] args) {
+  static void main(final String[] args) {
 
 
     final Sfx sfx = new Sfx(Loader.loadDirectory("characters/haschel/sounds/combat"));
