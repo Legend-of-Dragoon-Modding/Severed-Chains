c279745c73c667c0b51c77fa332c27e38525974a
Francis Dionne <96087943+FrancisDionne@users.noreply.github.com>
2026-02-26 20:54:04 -0800

    Fixed mouseClick params

--- src/main/java/legend/game/inventory/screens/CreditsScreen.java
+++ src/main/java/legend/game/inventory/screens/CreditsScreen.java
@@ -248,7 +248,7 @@ public class CreditsScreen extends MenuScreen {
   }
 
   @Override
-  protected InputPropagation mouseClick(final int x, final int y, final int button, final Set<InputMod> mods) {
+  protected InputPropagation mouseClick(final double x, final double y, final int button, final Set<InputMod> mods) {
     if(super.mouseClick(x, y, button, mods) == InputPropagation.HANDLED) {
       return InputPropagation.HANDLED;
     }
