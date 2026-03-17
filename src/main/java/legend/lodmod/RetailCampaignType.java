63edb9d1129fe6c6d99991f464cfc0a2424a8195
Corey Frenette <lordmonoxide@gmail.com>
2026-03-17 20:47:18 -0300

    Fix chars all starting at 0 xp

--- src/main/java/legend/lodmod/RetailCampaignType.java
+++ src/main/java/legend/lodmod/RetailCampaignType.java
@@ -24,37 +24,14 @@ public class RetailCampaignType extends CampaignType {
     final CharacterData2c kongol = gameState.addCharacter(LodCharacterTemplates.KONGOL.get().make(gameState));
     final CharacterData2c miranda = gameState.addCharacter(LodCharacterTemplates.MIRANDA.get().make(gameState));
 
-    for(int i = 1; i < 3; i++) {
-      lavitz.template.applyLevelUp(lavitz, null);
-    }
-
-    for(int i = 1; i < 4; i++) {
-      shana.template.applyLevelUp(shana, null);
-    }
-
-    for(int i = 1; i < 8; i++) {
-      rose.template.applyLevelUp(rose, null);
-    }
-
-    for(int i = 1; i < 13; i++) {
-      haschel.template.applyLevelUp(haschel, null);
-    }
-
-    for(int i = 1; i < 15; i++) {
-      albert.template.applyLevelUp(albert, null);
-    }
-
-    for(int i = 1; i < 17; i++) {
-      meru.template.applyLevelUp(meru, null);
-    }
-
-    for(int i = 1; i < 19; i++) {
-      kongol.template.applyLevelUp(kongol, null);
-    }
-
-    for(int i = 1; i < 23; i++) {
-      miranda.template.applyLevelUp(miranda, null);
-    }
+    this.levelUp(lavitz, 3);
+    this.levelUp(shana, 4);
+    this.levelUp(rose, 8);
+    this.levelUp(haschel, 13);
+    this.levelUp(albert, 15);
+    this.levelUp(meru, 17);
+    this.levelUp(kongol, 19);
+    this.levelUp(miranda, 23);
 
     gameState.charData_32c.get(0).partyFlags_04 = 0x3;
 
@@ -62,72 +39,54 @@ public class RetailCampaignType extends CampaignType {
     gameState.items_2e9.give(LodItems.HEALING_POTION.get());
     gameState.items_2e9.give(LodItems.HEALING_POTION.get());
 
-    dart.stats.getStat(HP_STAT.get()).restore();
-    dart.stats.getStat(MP_STAT.get()).restore();
     dart.equip(EquipmentSlot.WEAPON, LodEquipment.BROAD_SWORD.get());
     dart.equip(EquipmentSlot.HELMET, LodEquipment.BANDANA.get());
     dart.equip(EquipmentSlot.ARMOUR, LodEquipment.LEATHER_ARMOR.get());
     dart.equip(EquipmentSlot.BOOTS, LodEquipment.LEATHER_BOOTS.get());
     dart.equip(EquipmentSlot.ACCESSORY, LodEquipment.BRACELET.get());
 
-    lavitz.stats.getStat(HP_STAT.get()).restore();
-    lavitz.stats.getStat(MP_STAT.get()).restore();
     lavitz.equip(EquipmentSlot.WEAPON, LodEquipment.SPEAR.get());
     lavitz.equip(EquipmentSlot.HELMET, LodEquipment.SALLET.get());
     lavitz.equip(EquipmentSlot.ARMOUR, LodEquipment.SCALE_ARMOR.get());
     lavitz.equip(EquipmentSlot.BOOTS, LodEquipment.LEATHER_BOOTS.get());
     lavitz.equip(EquipmentSlot.ACCESSORY, LodEquipment.BRACELET.get());
 
-    shana.stats.getStat(HP_STAT.get()).restore();
-    shana.stats.getStat(MP_STAT.get()).restore();
     shana.equip(EquipmentSlot.WEAPON, LodEquipment.SHORT_BOW.get());
     shana.equip(EquipmentSlot.HELMET, LodEquipment.FELT_HAT.get());
     shana.equip(EquipmentSlot.ARMOUR, LodEquipment.CLOTHES.get());
     shana.equip(EquipmentSlot.BOOTS, LodEquipment.LEATHER_SHOES.get());
     shana.equip(EquipmentSlot.ACCESSORY, LodEquipment.BRACELET.get());
 
-    rose.stats.getStat(HP_STAT.get()).restore();
-    rose.stats.getStat(MP_STAT.get()).restore();
     rose.equip(EquipmentSlot.WEAPON, LodEquipment.RAPIER.get());
     rose.equip(EquipmentSlot.HELMET, LodEquipment.FELT_HAT.get());
     rose.equip(EquipmentSlot.ARMOUR, LodEquipment.LEATHER_JACKET.get());
     rose.equip(EquipmentSlot.BOOTS, LodEquipment.LEATHER_SHOES.get());
     rose.equip(EquipmentSlot.ACCESSORY, LodEquipment.BRACELET.get());
 
-    haschel.stats.getStat(HP_STAT.get()).restore();
-    haschel.stats.getStat(MP_STAT.get()).restore();
     haschel.equip(EquipmentSlot.WEAPON, LodEquipment.IRON_KNUCKLE.get());
     haschel.equip(EquipmentSlot.HELMET, LodEquipment.ARMET.get());
     haschel.equip(EquipmentSlot.ARMOUR, LodEquipment.DISCIPLE_VEST.get());
     haschel.equip(EquipmentSlot.BOOTS, LodEquipment.IRON_KNEEPIECE.get());
     haschel.equip(EquipmentSlot.ACCESSORY, LodEquipment.BRACELET.get());
 
-    albert.stats.getStat(HP_STAT.get()).restore();
-    albert.stats.getStat(MP_STAT.get()).restore();
     albert.equip(EquipmentSlot.WEAPON, LodEquipment.SPEAR.get());
     albert.equip(EquipmentSlot.HELMET, LodEquipment.SALLET.get());
     albert.equip(EquipmentSlot.ARMOUR, LodEquipment.SCALE_ARMOR.get());
     albert.equip(EquipmentSlot.BOOTS, LodEquipment.LEATHER_BOOTS.get());
     albert.equip(EquipmentSlot.ACCESSORY, LodEquipment.BRACELET.get());
 
-    meru.stats.getStat(HP_STAT.get()).restore();
-    meru.stats.getStat(MP_STAT.get()).restore();
     meru.equip(EquipmentSlot.WEAPON, LodEquipment.MACE.get());
     meru.equip(EquipmentSlot.HELMET, LodEquipment.TIARA.get());
     meru.equip(EquipmentSlot.ARMOUR, LodEquipment.SILVER_VEST.get());
     meru.equip(EquipmentSlot.BOOTS, LodEquipment.SOFT_BOOTS.get());
     meru.equip(EquipmentSlot.ACCESSORY, LodEquipment.BRACELET.get());
 
-    kongol.stats.getStat(HP_STAT.get()).restore();
-    kongol.stats.getStat(MP_STAT.get()).restore();
     kongol.equip(EquipmentSlot.WEAPON, LodEquipment.AXE.get());
     kongol.equip(EquipmentSlot.HELMET, LodEquipment.ARMET.get());
     kongol.equip(EquipmentSlot.ARMOUR, LodEquipment.LION_FUR.get());
     kongol.equip(EquipmentSlot.BOOTS, LodEquipment.IRON_KNEEPIECE.get());
     kongol.equip(EquipmentSlot.ACCESSORY, LodEquipment.BRACELET.get());
 
-    miranda.stats.getStat(HP_STAT.get()).restore();
-    miranda.stats.getStat(MP_STAT.get()).restore();
     miranda.equip(EquipmentSlot.WEAPON, LodEquipment.SHORT_BOW.get());
     miranda.equip(EquipmentSlot.HELMET, LodEquipment.FELT_HAT.get());
     miranda.equip(EquipmentSlot.ARMOUR, LodEquipment.CLOTHES.get());
@@ -135,6 +94,18 @@ public class RetailCampaignType extends CampaignType {
     miranda.equip(EquipmentSlot.ACCESSORY, LodEquipment.BRACELET.get());
 
     gameState.gold_94 = 20;
+  }
+
+  private void levelUp(final CharacterData2c character, final int level) {
+    while(character.level_12 < level - 1) {
+      character.applyLevelUp(null);
+    }
+
+    character.xp_00 = character.getXpToNextLevel();
+    character.applyLevelUp(null);
+
+    character.stats.getStat(HP_STAT.get()).restore();
+    character.stats.getStat(MP_STAT.get()).restore();
   }
 
   @Override
