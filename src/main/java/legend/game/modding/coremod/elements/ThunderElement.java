01089d626dc5973884338d7ddad4030c0321c6ce
Corey Frenette <lordmonoxide@gmail.com>
2026-03-21 22:19:56 -0300

    Fix elemental hit effect being invisible and add handling for water element weapons

--- src/main/java/legend/game/modding/coremod/elements/ThunderElement.java
+++ src/main/java/legend/game/modding/coremod/elements/ThunderElement.java
@@ -1,7 +1,6 @@
 package legend.game.modding.coremod.elements;
 
 import legend.game.characters.Element;
-import legend.game.combat.types.AttackType;
 import org.joml.Vector3f;
 
 public class ThunderElement extends Element {
@@ -10,20 +9,17 @@ public class ThunderElement extends Element {
   }
 
   @Override
-  public int adjustAttackingElementalDamage(final AttackType attackType, final int damage, final Element targetElement) {
-    if(targetElement == this) {
-      return damage * 50 / 100;
-    }
-
-    return damage;
+  public boolean isStrongAgainst(final Element other) {
+    return false;
   }
 
   @Override
-  public int adjustDragoonSpaceDamage(final AttackType attackType, final int damage, final Element attackingElement) {
-    if(attackingElement == this) {
-      return damage * 150 / 100;
-    }
+  public boolean isWeakAgainst(final Element other) {
+    return other == this;
+  }
 
-    return damage;
+  @Override
+  public boolean boostsElement(final Element other) {
+    return other == this;
   }
 }
