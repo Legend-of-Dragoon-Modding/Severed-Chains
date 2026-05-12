ac476475fe7116ab35d1872efc54d040a8bc4d76
Corey Frenette <lordmonoxide@gmail.com>
2026-05-11 21:41:25 -0300

    Add inputs for everything SDL supports (#2662)

--- src/main/java/legend/core/platform/input/InputButton.java
+++ src/main/java/legend/core/platform/input/InputButton.java
@@ -15,6 +15,18 @@ public enum InputButton {
   RIGHT_BUMPER(InputCodepoints.RIGHT_BUMPER),
   LEFT_STICK(InputCodepoints.LEFT_STICK),
   RIGHT_STICK(InputCodepoints.RIGHT_STICK),
+  GUIDE(InputCodepoints.XBOX_BUTTON_GUIDE),
+  TOUCHPAD(InputCodepoints.PS_BUTTON_TOUCHPAD),
+  LEFT_PADDLE1(InputCodepoints.GENERIC_LEFT_PADDLE1),
+  RIGHT_PADDLE1(InputCodepoints.GENERIC_RIGHT_PADDLE1),
+  LEFT_PADDLE2(InputCodepoints.GENERIC_LEFT_PADDLE2),
+  RIGHT_PADDLE2(InputCodepoints.GENERIC_RIGHT_PADDLE2),
+  MISC1(InputCodepoints.GENERIC_MISC1),
+  MISC2(InputCodepoints.GENERIC_MISC2),
+  MISC3(InputCodepoints.GENERIC_MISC3),
+  MISC4(InputCodepoints.GENERIC_MISC4),
+  MISC5(InputCodepoints.GENERIC_MISC5),
+  MISC6(InputCodepoints.GENERIC_MISC6),
   ;
 
   public final char codepoint;
