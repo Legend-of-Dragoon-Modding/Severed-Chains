d225ccf616d05c8c342a9af7bb5b746d103a9968
Patrick Kolb <64711466+pkolb-dev@users.noreply.github.com>
2026-05-12 11:22:09 -0500

    remove unused imports

--- src/main/java/legend/game/modding/events/characters/PostCharacterDragoonLevelUpEvent.java
+++ src/main/java/legend/game/modding/events/characters/PostCharacterDragoonLevelUpEvent.java
@@ -1,10 +1,6 @@
 package legend.game.modding.events.characters;
 
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import legend.game.characters.CharacterData2c;
-import legend.game.characters.StatType;
-import org.legendofdragoon.modloader.events.CancelableEvent;
 import org.legendofdragoon.modloader.events.Event;
 
 public class PostCharacterDragoonLevelUpEvent extends Event {
