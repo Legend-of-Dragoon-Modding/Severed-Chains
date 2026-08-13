c9fac176bf911c69fae3ee9a84bfc64f36414981
Ink230 <justinquinn230@gmail.com>
2026-08-13 15:39:50 -0400

    add player spells consistent resolution and description surfaces

--- src/main/java/legend/game/modding/events/battle/SpellStatsEvent.java
+++ src/main/java/legend/game/modding/events/battle/SpellStatsEvent.java
@@ -3,13 +3,22 @@ package legend.game.modding.events.battle;
 import legend.game.characters.CharacterData2c;
 import legend.game.inventory.SpellStats0c;
 import org.legendofdragoon.modloader.events.Event;
+import org.legendofdragoon.modloader.registries.RegistryId;
 
 public class SpellStatsEvent extends Event {
   public final CharacterData2c character;
+  public final RegistryId spellId;
+  public final SpellStats0c baseSpell;
   public SpellStats0c spell;
 
   public SpellStatsEvent(final CharacterData2c character, final SpellStats0c spell) {
+    this(character, spell.getRegistryId(), spell);
+  }
+
+  public SpellStatsEvent(final CharacterData2c character, final RegistryId spellId, final SpellStats0c baseSpell) {
     this.character = character;
-    this.spell = spell;
+    this.spellId = spellId;
+    this.baseSpell = baseSpell;
+    this.spell = baseSpell;
   }
 }
