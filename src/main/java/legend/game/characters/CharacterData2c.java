2584a6d8d71c7f93e0e25b58f73e8381c438ab5c
Ink230 <justinquinn230@gmail.com>
2026-08-17 05:42:50 -0400

    add additions contextual resolution and defensive hit copies

--- src/main/java/legend/game/characters/CharacterData2c.java
+++ src/main/java/legend/game/characters/CharacterData2c.java
@@ -2,11 +2,13 @@ package legend.game.characters;
 
 import legend.core.lang.I18nText;
 import legend.core.lang.TextComponent;
+import legend.game.additions.Addition;
 import legend.game.additions.AdditionHits80;
 import legend.game.combat.bent.PlayerBattleEntity;
 import legend.game.inventory.CanEquip;
 import legend.game.inventory.Equipment;
 import legend.game.inventory.EquipmentTypes;
+import legend.game.modding.events.characters.ResolveAdditionEvent;
 import legend.game.modding.events.characters.ResolveCharacterElementEvent;
 import legend.game.types.EquipmentSlot;
 import legend.game.types.GameState52c;
@@ -24,6 +26,7 @@ import java.util.Map;
 import java.util.Set;
 
 import static legend.core.GameEngine.EVENTS;
+import static legend.core.GameEngine.REGISTRIES;
 
 public class CharacterData2c {
   public static final int IN_PARTY = 0x1;
@@ -252,6 +255,12 @@ public class CharacterData2c {
 
   public CharacterAdditionInfo getAdditionInfo(final RegistryId id) {
     return this.additions.get(id);
+  }
+
+  public Addition resolveAddition(final RegistryId id) {
+    final Addition baseAddition = REGISTRIES.additions.getEntry(id).get();
+    final CharacterAdditionInfo additionInfo = this.getAdditionInfo(id);
+    return EVENTS.postEvent(new ResolveAdditionEvent(this, additionInfo, id, baseAddition)).addition;
   }
 
   public CharacterAdditionInfo addAddition(final RegistryId id, final CharacterAdditionInfo info) {
