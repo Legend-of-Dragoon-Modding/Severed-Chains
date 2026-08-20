a2baeead3ec49f91b3ac9e0417ba42c202508502
Corey Frenette <LordMonoxide@users.noreply.github.com>
2026-08-20 19:06:28 -0300

    Apply suggestion from @LordMonoxide

--- src/main/java/legend/game/inventory/screens/SaveGameScreen.java
+++ src/main/java/legend/game/inventory/screens/SaveGameScreen.java
@@ -184,7 +184,9 @@ public class SaveGameScreen extends MenuScreen {
   }
 
   private void menuDelete() {
-    if(this.closing) return;
+    if(this.closing) {
+      return;
+    }
 
     playMenuSound(40);
 
